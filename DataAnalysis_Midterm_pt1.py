9fe0677fa53b4d72a5fd893957879dcefb752e94
Selina Davila Olivera <sdaolivera@gmail.com>
2023-11-13 13:25:08 -0800

    Update DataAnalysis_Midterm_pt1.py

--- DataAnalysis_Midterm_pt1.py
+++ DataAnalysis_Midterm_pt1.py
@@ -12,11 +12,11 @@ import pandas as pd
 C = 6000 #kPa ## I can vary this between 5kPa to 9kPa
 pw = 1000 # kg/m3
 ps = 1800 # kg/m3c
-phi = np.deg2rad(40)  # converts phi to radians ## set to 42 right now
+phi = np.deg2rad(38)  # converts phi to radians ## set to 42 right now
 g = 9.81  # m/s2
 K_avg = 0.004 # m2/yr
 Sc = 1.25
-slope_ang = np.arange(27,51, 0.1)
+slope_ang = np.arange(30,50)
 slope_rad = np.deg2rad(slope_ang)
 hollow_rad = (np.arctan((0.8*(np.tan(np.deg2rad(slope_ang))))))
 hollow_ang = np.rad2deg(np.arctan((0.8*(np.tan(np.deg2rad(slope_ang))))))
@@ -50,5 +50,5 @@ plt.legend()
 plt.xlabel('Hollow Slope (degrees)')
 plt.ylabel('Recurrance Interval (yrs)')
 
-
+B = (((np.cos(hollow_rad))**(1/2))*(((np.tan(slope_rad))**2)-((np.tan(hollow_rad))**2))**(1/4)) * ((((np.cos(hollow_rad)**2)/(np.cos(slope_rad)**2))-1)**(1/4))
 
