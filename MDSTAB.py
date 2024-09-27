3fadf60fd48d66e118f3b485b250d9a0242c2379
Selina Davila Olivera <116762187+sdavilao00@users.noreply.github.com>
2024-09-27 10:41:12 -0700

    Updates with parameter formulas

--- MDSTAB.py
+++ MDSTAB.py
@@ -33,12 +33,12 @@ yw = g*pw
 ys = g*ps
 phi = np.deg2rad(41)  # converts phi to radians ## set to 42 right now
 #z = np.arange(0,5,0.1)
-z = 5
+z = 2.5
 
 # Slope stability variables
 m = 1 # m # saturation ration (h/z)
-l = 15 # m # length
-w = 7 # m # width
+l = 10 # m # length
+w = 5 # m # width
 C0 = 6.8 # kPa
 j = 4.96
 
@@ -50,30 +50,33 @@ hollow_ang = np.rad2deg(np.arctan((0.8*(np.tan(np.deg2rad(slope_ang)))))) # Holl
 
 #%% Calculated variables for MDSTAB
 
-# Earth Pressure Variables
-K0 = 1-(np.sin(hollow_rad))
-Ka = 0.1
-Kp = 4
-
 #Cohesion variables
 Crb = C0*2.718281**(-z*j)
 Crl = (C0/(j*z))*(1 - 2.718281**(-z*j))
 # Crb = 5 # kPa # Cohesion of roots on base
 # Crl = 5 # kPa # Lateral root cohesion
 
+
+
+# Earth Pressure Variables
+K0 = 1-(np.sin(hollow_rad))
+Ka = 0.3
+Kp = 6
+
+
 #%% MDSTAB
 
 #Basal resistence force of the slide block
-Frb = (Crb + ((np.cos(hollow_rad))**2)*z*((ys-yw)*m)*np.tan(phi))*l*w
+Frb = (Crb + ((np.cos(hollow_rad))**2)*z*(ys-yw*m)*np.tan(phi))*l*w
 
 # Resisting force of each cross slope side of the slide block (2 lateral margins)
-Frc = (Crl + (K0*0.5*z*((ys-yw)*m**2)*np.tan(phi)))*(np.cos(hollow_rad)*z*l*2)
+Frc = (Crl + (K0*0.5*z*(ys-yw*m**2)*np.tan(phi)))*(np.cos(hollow_rad)*z*l*2)
 
 # Slope parallel component of passive force - slope-parallel component of active force
-Frddu = (Kp-Ka)*0.5*z**2*(ys-yw*m**2)*w*((np.cos((2*phi) - hollow_rad))/(np.cos(phi)))
+Frddu = (Kp-Ka)*0.5*(z**2)*(ys-yw*(m**2))*w
 
 # Central block driving force
-Fdc = (np.sin(hollow_rad))*(np.cos(hollow_rad))*z*ps*l*w
+Fdc = (np.sin(hollow_rad))*(np.cos(hollow_rad))*z*ys*l*w
 
 #Factor of Safety calculation
 FS = (Frb + Frc + Frddu)/Fdc
@@ -84,3 +87,39 @@ plt.figure()
 plt.scatter(hollow_ang, FS)
 plt.xlabel('Hollow Angle')
 plt.ylabel('Factor of Safety')
+
+#%%
+#Set FS to 1 and solve for length where w = l*0.5
+
+y = 4*(Crl + (K0*0.5*z*(ys-yw*m**2)*np.tan(phi)))*(np.cos(hollow_rad)*z)
+b = (Kp-Ka)*0.5*(z**2)*(ys-yw*(m**2))
+x = (Crb + ((np.cos(hollow_rad))**2)*z*(ys-yw*m)*np.tan(phi))
+a = (np.sin(hollow_rad))*(np.cos(hollow_rad))*z*ys
+
+length = (x+b)/(a-x)
+
+
+
+plt.figure()
+plt.scatter(hollow_ang, length)
+plt.xlabel('Hollow Angle')
+plt.ylabel('Length (m)')
+
+#%% Area
+
+A = (2*Crl*z + K0*z**2*(ys-yw*m**2)*np.tan(phi))*np.cos(hollow_rad)*(l/w)**0.5
+B = (Kp-Ka)*0.5*z**2*(ys-ys*m**2)*(l/w)**(-0.5)
+C = (np.sin(hollow_rad)*np.cos(hollow_rad)*z*ys) - Crb - (((np.cos(hollow_rad))**2)*z*(ys-yw*m)*np.tan(phi))
+
+
+Ac = (A + B)/C
+
+
+
+
+
+
+
+
+
+
